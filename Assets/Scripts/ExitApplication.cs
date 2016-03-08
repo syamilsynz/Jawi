5c1027d10a26ad4fef567af3c7fb71e79043ec93
Muhammad Syamil <syamilmuhd90@yahoo.com>
2016-03-09 00:00:52 +0800

    apply keystore ingeniworks
    - added flash card jawi
    - apply keystore igw for publishing

---
+++ Assets/Scripts/ExitApplication.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitApplication : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            #if UNITY_STANDALONE
+            //Quit the application
+            Application.Quit();
+            #endif
+
+            //If we are running in the editor
+            #if UNITY_EDITOR
+            //Stop playing the scene
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+
+            Application.Quit();
+        }
+	}
+}
